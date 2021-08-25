487fe8722dcbeda2651d0592a0c2c4caf20b6a71
cliftonrichey <cliftonrichey@users.noreply.github.com>
2021-08-25 19:38:06 +0000

    Understanding Python 2.x vs Python 3.x

--- broken01/ifixed.py
+++ broken01/ifixed.py
@@ -7,20 +7,20 @@ calc2 = 0.0
 operation = ""
 while True: (calc1 != "q")
 print("What is the first operator? Or, enter q to quit: ")
-    calc1 = input()
-    if calc1 == "Q":
+calc1 = input()
+if calc1 == "q":
         break
-    calc1 = float(calc1)
-print("What is the second operator? Or, enter q to quit: ")
-    calc2 = input()
-    if calc2 == "q":
+        calc1 = float(calc1)
+        print("What is the second operator? Or, enter q to quit: ")
+calc2 = input()
+if calc2 == "q":
         break
-    calc2 = float(calc2)
-print("Enter an operation to perform on the two operators (+ or -): ")
+        calc2 = float(calc2)
+        print("Enter an operation to perform on the two operators (+ or -): ")
     operation = input()
-    if operation == "+":
-print(\n + str(calc1) + " + " + str(calc2) + " = " + str(calc1 + calc2))
-    ifel operation == '-':
-print("\n" + str(calc1) + " - " + str(calc2) + " = " + str(calc1 - calc2))
-    else:
-print("Not a valid entry. Restarting...")
+if operation == "+":
+    print("\n" + str(calc1) + " + " + str(calc2) + " = " + str(calc1 + calc2))
+elif operation == '-':
+    print("\n" + str(calc1) + " - " + str(calc2) + " = " + str(calc1 - calc2))
+else:
+    print("Not a valid entry. Restarting...")
