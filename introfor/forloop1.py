cb567a17a4f14045cb0dc403ff43549906dcad4d
cliftonrichey <cliftonrichey@users.noreply.github.com>
2021-08-25 21:34:48 +0000

    studying for logic

---
+++ introfor/forloop1.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""RZFeeser | Alta3 Research
+   learning about for logic"""
+
+# create the list called vendors
+vendorList = ["cisco", "juniper", "big_ip", "f5", "arista"]
+# loop across the list vendors
+for vendor in vendorList:
+    print("The vendor is:" + vendor)  # each time through the loop print value of x
+print("\nOur loop has ended.")  # when the loop ends print this
+
